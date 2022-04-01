c0e5791abcf83be818bbd012c4e543a0b5110ace
Pbaodoge <90094025+Pbaodoge@users.noreply.github.com>
2022-04-01 17:18:33 +0700

    updated some tricks

--- functions.js
+++ functions.js
@@ -3,6 +3,9 @@ function preventhttp(){
          location.replace(`https://${window.location.href}`);
 }
 function rickroll(){
-    for(let i = 0; i <= 4; i++)
-         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
+    setTimeout(() => {  
+        location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'); 
+    }, 6000);
+         
 }
