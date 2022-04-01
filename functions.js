51bf7942675d713f59515e6be8c001b34e25d0e1
Pbaodoge <90094025+Pbaodoge@users.noreply.github.com>
2022-04-01 18:31:19 +0700

    Added a logo | Siesta :P

--- functions.js
+++ functions.js
@@ -3,9 +3,14 @@ function preventhttp(){
          location.replace(`https://${location.hostname}${location.pathname}`);
 }
 function rickroll(){
-    window.open(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`);
     setTimeout(() => {  
-        location.replace(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`); 
-    }, 3500);
+        setTimeout(() =>{
+            location.replace(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`);
+        }, 3000);
+        setTimeout(() =>{
+            window.open(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`);
+        }, 3000);
+        
+    }, 1000);
          
 }
