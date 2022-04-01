c0b38f3b500cd3d1649df5d8e30ce9c2b4d4e388
Pbaodoge <90094025+Pbaodoge@users.noreply.github.com>
2022-04-01 17:52:02 +0700

    nvm

--- functions.js
+++ functions.js
@@ -3,8 +3,7 @@ function preventhttp(){
          location.replace(`https://${location.hostname}${location.pathname}`);
 }
 function rickroll(){
-    for(let i = 1; i <= 10; i++)
-        window.open(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`);
+    window.open(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`);
     setTimeout(() => {  
         location.replace(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`); 
     }, 3500);
