f64c24ef91e08c664b531279ef7b43911d33acef
Pbaodoge <90094025+Pbaodoge@users.noreply.github.com>
2022-04-01 17:30:11 +0700

    Fix my stupidity

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 function preventhttp(){
     if(location.protocol == "http:") 
-         location.replace(`https://${window.location.href}`);
+         location.replace(`https://${location.hostname}${location.pathname}`);
 }
 function rickroll(){
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
+    window.open(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`);
     setTimeout(() => {  
-        location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'); 
+        location.replace(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`); 
     }, 6000);
          
 }
