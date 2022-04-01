07d3a24ee4ad7c809d96bbc6c5e84aca79534006
Pbaodoge <90094025+Pbaodoge@users.noreply.github.com>
2022-04-01 16:55:06 +0700

    minor fixes

--- functions.js
+++ functions.js
@@ -3,6 +3,6 @@ function preventhttp(){
          location.replace(`https://${window.location.href}`);
 }
 function rickroll(){
-    for(let i = 0; i <= 4; i++) 
+    for(let i = 0; i <= 4; i++)
          window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
-  
+}
