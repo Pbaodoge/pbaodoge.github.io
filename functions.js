af0ed156b54142484cf881e97e68f77c8041168f
Siesta <90094025+Pbaodoge@users.noreply.github.com>
2022-04-01 11:18:32 +0700

    hehehehehe
    April Fools hehehe

--- functions.js
+++ functions.js
@@ -2,3 +2,7 @@ function preventhttp(){
     if(location.protocol == "http:") 
          location.replace(`https://${window.location.href}`);
 }
+function rickroll(){
+    for(let i = 0; i <= 4; i++) 
+         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
+  
